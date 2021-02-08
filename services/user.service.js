41d8b287501eaebfa2fe227185094cebe288bd53
Gunjan Hardasmalani <hardasmalani.g@northeastern.edu>
2021-02-08 16:33:16 -0500

    Add get user information api

--- services/user.service.js
+++ services/user.service.js
@@ -28,3 +28,30 @@ exports.createUser = (request, res) => {
       });
   
   };
+
+  //get user details by passing id to the request
+  exports.getUserWithId = (req, res) => {
+ 
+
+    User.findById(req.params.id)
+      .then(user => {
+        if (!user) {
+          
+          return res.status(404).send({ message: "User Not found." });
+        }
+       
+        res.status(200).send({
+          id: user.id,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          username: user.username,
+          account_created: user.createdAt,
+          account_updated:user.updatedAt
+        });
+       
+      })
+      .catch(err => {
+        res.status(400).send({ message: err.message });
+      });
+     
+  };
