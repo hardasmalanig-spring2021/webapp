58d442db5522e752eb86530fb00dfcb8f2005502
Gunjan Hardasmalani <hardasmalani.g@northeastern.edu>
2021-02-08 18:52:11 -0500

    Add update user api

--- services/user.service.js
+++ services/user.service.js
@@ -55,3 +55,35 @@ exports.createUser = (request, res) => {
       });
      
   };
+
+  //Update User Information using basicAUth
+exports.updateUser = (req, res) => {
+
+  User.update({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    password: bcrypt.hashSync(req.body.password, 8)
+  }, {
+    where: {
+      username: req.user.username
+    },
+  }).then(() => {
+    User.findOne({
+      where: {
+        username: req.user.username
+      }
+    }).then(user => {
+      
+      res.send({
+        firstname: user.first_name,
+        last_name: user.last_name,
+        username: user.username,
+      });
+     
+    })
+  })
+    .catch(err => {
+      res.status(400).send({ message: err.message });
+    });
+    
+};
