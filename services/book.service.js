8023f27e1b87f135cd20224a5095f6a35032b364
Gunjan Hardasmalani <hardasmalani.g@northeastern.edu>
2021-03-24 03:22:36 -0400

    Add logs and metrics for users and books

--- services/book.service.js
+++ services/book.service.js
@@ -5,16 +5,29 @@ const User = db.user;
 const File = db.file;
 const s3 = require("../config/s3.config");
 const deleteParams = s3.deleteParams;
+var SDC = require('statsd-client');
+Metrics = new SDC({port: 8125});
+const log = require("../log")
+const logger = log.getLogger('logs');
+
+
 //create book with basic auth
 exports.createBook = (req, res) => {
+  Metrics.increment('book.POST.createBook');
+  let api_timer = new Date();
+  logger.info("Create Book");
+  let db_timer = new Date();
     User.findOne({
         where: {
             username: req.user.username
         }
     }).then(user => {
+      Metrics.timing('db.book.POST.createBook.findUser',db_timer);
         if (!user) {
+            logger.error("Error while creating the book, User Not found");
             return res.status(404).send({ message: "User Not found" });
         } else {
+          let db_timer1 = new Date();
             Book.create({
                 title: req.body.title,
                 author: req.body.author,
@@ -22,6 +35,7 @@ exports.createBook = (req, res) => {
                 published_date: req.body.published_date,
                 user_id: user.id
             }).then(book => {
+              Metrics.timing('db.book.POST.createBook',db_timer1);
                 res.status(201).send({
                     id: book.id,
                     title: book.title,
@@ -32,7 +46,9 @@ exports.createBook = (req, res) => {
                     user_id: book.user_id,
                     book_images: book.book_images || []
                 });
+                Metrics.timing('api.user.POST.createBook',api_timer);
             }).catch(err => {
+                logger.error("Error while creating the book",err.message);
                 res.status(400).send({ message: err.message });
             });
         }
@@ -44,10 +60,17 @@ exports.createBook = (req, res) => {
 
 //Find book by id with basic auth
 exports.getBook = (req, res) => {
+    Metrics.increment('book.GET.getBookByID');
+    let api_timer = new Date();
+    logger.info("Get book by ID");
+    let db_timer = new Date();
     Book.findByPk(req.params.id).then(book => {
+      Metrics.timing('api.book.GET.getBookByID',db_timer);
         if (!book) {
+            logger.error("Error while getting the book, Book Not found");
             return res.status(404).send({ message: "Book Not found." });
         }
+        
         res.status(200).send({
             id: book.id,
             title: book.title,
@@ -58,29 +81,41 @@ exports.getBook = (req, res) => {
             user_id: book.user_id,
             book_images: book.book_images || [],
         });
-
+        Metrics.timing('api.book.GET.getBookByID',api_timer);
     }).catch(err => {
+      logger.error("Error while getting the bookdetails",err.message);
         res.status(400).send({ message: err.message });
     });
 }
 
 exports.deleteBook = (req, res) => {
+  Metrics.increment('book.DELETE.deleteBook');
+  let api_timer = new Date();
+  logger.info("elete Book by Id");
+  let db_timer = new Date();
     Book.findByPk(req.params.id).then((book) => {
+      Metrics.timing('db.book.DELETE.deleteBook.findBook',db_timer);
       if (!book) {
+        logger.error("Error deleting the book, Book Not found");
         return res
           .status(404)
           .send({ message: "Request Failed, Book Not Found" });
       } else if (book.user_id != req.user.id) {
+        logger.error("Error Deleteing book, User is not authorized to delete the book")
         return res
           .status(401)
           .send({ message: "Request Failed Unauthorized User." });
       } else {
+        let db_timer_file = new Date();
+        
         File.findAll({
           raw: true,
           where: {
             book_id: req.params.id,
           },
         }).then((files) => {
+          Metrics.timing('db.book.DELETE.deleteBook',db_timer_file);
+          let s3_timer = new Date();
           for (let file of files) {
             deleteParams.Key = file.s3_object_name;
             s3.s3Client.deleteObject(deleteParams, (err) => {
@@ -91,6 +126,8 @@ exports.deleteBook = (req, res) => {
               }
             });
           }
+          Metrics.timing('s3.book.DELETE.deleteBook',s3_timer);
+          let db_timer = new Date();
           Book.destroy({
             where: {
               id: req.params.id,
@@ -98,9 +135,12 @@ exports.deleteBook = (req, res) => {
             },
           })
             .then(() => {
+              Metrics.timing('db.book.DELETE.deleteBook',db_timer);
               res.status(204).send();
+              Metrics.timing('api.book.DELETE.deleteBook',api_timer);
             })
             .catch((err) => {
+              logger.error("Error deleting the book",err.message);
               res.status(400).send({ message: err.message });
             });
         });
@@ -110,13 +150,21 @@ exports.deleteBook = (req, res) => {
 
 //get all books
 exports.getAllBooks = (req, res) => {
+  Metrics.increment('book.GET.getAllBooks');
+  let api_timer = new Date();
+  logger.info("Get All books ");
+  let db_timer = new Date();
     Book.findAll({ raw: true }).then(book => {
+      Metrics.timing('api.book.GET.getAllBooks',db_timer);
         if (!book) {
+          logger.error("Error when trying to get all the books, No books in database");
             return res.status(404).send({ message: "There are no books in the database" });
         }
         res.status(200).send(book);
+        Metrics.timing('api.book.GET.getAllBooks',api_timer);
 
     }).catch(err => {
+      logger.error("Error when trying to get all the books",err.message);
         res.status(400).send({ message: err.message });
     });
 }
--- services/user.service.js
+++ services/user.service.js
@@ -1,16 +1,24 @@
 const db = require("../loaders/db.loader");
 const User = db.user;
 var bcrypt = require("bcryptjs");
+var SDC = require('statsd-client');
+Metrics = new SDC({port: 8125});
+const log = require("../log")
+const logger = log.getLogger('logs');
 
 //Create user route
 exports.createUser = (request, res) => {
+  Metrics.increment('user.POST.createUser');
+  let api_timer = new Date()
+  logger.info("Create User");
+  let db_timer = new Date()
   User.create({
     first_name: request.body.first_name,
     last_name: request.body.last_name,
     username: request.body.username,
     password: bcrypt.hashSync(request.body.password, 8)
   }).then(user => {
-
+    Metrics.timing('db.user.POST.createUser',db_timer);
     res.status(201).send({
       id: user.id,
       firstname: user.first_name,
@@ -19,10 +27,13 @@ exports.createUser = (request, res) => {
       account_created: user.createdAt,
       account_updated: user.updatedAt,
     });
+    Metrics.timing('api.user.POST.createUser',api_timer);
 
   })
     .catch(err => {
+      logger.error("Error creating the user",err.message );
       res.status(400).send({ message: err.message });
+      
     });
 
 };
@@ -31,13 +42,19 @@ exports.createUser = (request, res) => {
 
 // Get User Information using Authentication
 exports.getUserWithBasicAuth = (req, res) => {
+  Metrics.increment('user.GET.getUserWithBasicAuth');
+  let api_timer = new Date()
+  logger.info("Get User with Basic Auth");
+  let db_timer = new Date()
   User.findOne({
     where: {
       username: req.user.username
     }
   })
     .then(user => {
+      Metrics.timing('db.user.GET.getUserWithBasicAuth',db_timer);
       if (!user) {
+        logger.error("Error User not found" );
         return res.status(404).send({ message: "User Not found" });
       }
 
@@ -49,9 +66,10 @@ exports.getUserWithBasicAuth = (req, res) => {
         account_created: user.createdAt,
         account_updated: user.updatedAt
       });
-
+      Metrics.timing('api.user.GET.getUserWithBasicAuth',api_timer);
     })
     .catch(err => {
+      logger.error("Error getting the user",err.message );
       res.status(400).send({ message: err.message });
     });
 
@@ -59,6 +77,10 @@ exports.getUserWithBasicAuth = (req, res) => {
 
 //Update User Information using basicAUth
 exports.updateUser = (req, res) => {
+  Metrics.increment('user.PUT.updateUser');
+  let api_timer = new Date();
+  logger.info("Update User Information");
+  let db_timer = new Date();
 
   const requestBody = {};
   if (req.body.first_name) {
@@ -84,10 +106,13 @@ exports.updateUser = (req, res) => {
           username: req.user.username
         }
       }).then(user => {
+        Metrics.timing('db.user.PUT.updateUser',db_timer);
         res.status(204).send();
       })
+      Metrics.timing('api.user.PUT.updateUser',api_timer);
     })
     .catch(err => {
+      logger.error("Error updating user information",err.message );
       res.status(400).send({ message: err.message });
     });
 
